b05cf22c1656307f19229a8bbfefa5a13d501163
Andy-Castle <masterchief_and@live.com>
2025-03-29 20:04:32 -0500

    resolvi el Sum Square Difference del projecto Euler

--- Ejercicio 6/SumSquareDifference.js
+++ Ejercicio 6/SumSquareDifference.js
@@ -1,7 +1,21 @@
 function squareOfThefirstOneHundredNaturalNumbers(number) {
+  let result = 0;
   for (let i = 1; i <= number; i++) {
-    console.log(i);
+    result += Math.pow(i, 2);
   }
+  return result;
 }
 
-squareOfThefirstOneHundredNaturalNumbers(100);
+let sumOfSquaresFirstOneHundred = squareOfThefirstOneHundredNaturalNumbers(100);
+
+function squareOfThefirstOneHundredNaturalNumbersSUM(number) {
+  let result = 0;
+  for (let i = 1; i <= number; i++) {
+    result += i;
+  }
+  return Math.pow(result, 2);
+}
+
+let squareSumFirstOneHundred = squareOfThefirstOneHundredNaturalNumbersSUM(100);
+
+console.log(squareSumFirstOneHundred - sumOfSquaresFirstOneHundred);

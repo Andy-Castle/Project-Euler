9eb6539de80a4f672c69bdccab89ee6472f7a63d
Andy-Castle <masterchief_and@live.com>
2025-04-29 20:43:42 -0500

    estoy resolviendo esto, pero en partes, porque alparecer es complicado hacerlo por un codigo unico.

---
+++ Ejercicio 4/Largest Palindrome Product.js
@@ -0,0 +1,36 @@
+/*
+Un número palindrómico se lee igual en ambos sentidos. El mayor palindromo
+formado por el producto de dos cifras(2) es 9009 = 91 X 99
+
+Halla el palindromo mas grande formado por el producto de dos numero
+de 3 digitos
+*/
+
+function ItsPalindrome(value) {
+  let valueReverse = value.split("").reverse().join("");
+  if (valueReverse == value) {
+    return true;
+  }
+  return false;
+}
+
+let count = 100;
+let result = 0;
+let thisI = 0;
+let condition = true;
+
+function MultiplyValueX(value) {
+  for (let i = 100; i <= 999; i++) {
+    result = i * value;
+    let palindrome = ItsPalindrome(String(result));
+    if (palindrome) {
+      
+      return [i];
+    }
+    // console.log(`${i} x ${count} = ${result}`);
+  }
+}
+
+console.log(MultiplyValueX(count));
+
+// console.log(ItsPalindrome("9009"));
---
+++ Ejercicio 4/tempCodeRunnerFile.js
@@ -0,0 +1,22 @@
+/*
+Un número palindrómico se lee igual en ambos sentidos. El mayor palindromo
+formado por el producto de dos cifras(2) es 9009 = 91 X 99
+
+Halla el palindromo mas grande formado por el producto de dos numero
+de 3 digitos
+*/
+
+while (true) {
+  let count = 1;
+  let result = 0;
+
+  for (let i = 100; i <= 999; i++) {
+
+  }
+}
+
+function ItsPalindrome(value) {}
+
+console.log(ItsPalindrome("9009"));
+
+console.log("9099".split());
